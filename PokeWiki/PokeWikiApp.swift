53a17379997368af185cfdc19a43106ebca85d2b
hafizhMo <hafizh.hizb@gmail.com>
2023-08-26 21:55:37 +0700

    Initial Commit

---
+++ PokeWiki/PokeWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeWikiApp.swift
+//  PokeWiki
+//
+//  Created by Hafizh Mo on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
