99911040b244bae7427b8bfe1cd773f7a6f86c99
hafizhMo <hafizh.hizb@gmail.com>
2023-08-28 16:22:58 +0700

    add pokemon specs

--- PokeWiki/Data/Models/PokemonDetailDTO.swift
+++ PokeWiki/Data/Models/PokemonDetailDTO.swift
@@ -11,4 +11,18 @@ struct PokemonDetailDTO: Codable {
   let id: Int
   let height: Int
   let weight: Int
+  
+  var formatedHeight: String {
+      let dValue = Double(height)
+      let string = String(format: "%.2f", dValue / 10)
+      
+      return string
+  }
+  
+  var formatedWeight: String {
+    let dValue = Double(weight)
+    let string = String(format: "%.2f", dValue / 10)
+    
+    return string
+  }
 }
--- PokeWiki/Scenes/Detail/DetailView.swift
+++ PokeWiki/Scenes/Detail/DetailView.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 
 struct DetailView: View {
+  
+  @StateObject var viewModel: ViewModel
+  
+  init(viewModel: ViewModel = .init(), pokemon: PokemonDTO) {
+    _viewModel = StateObject(wrappedValue: viewModel)
+    self.pokemon = pokemon
+    
+    viewModel.getDetails(pokemon: pokemon)
+  }
+  
   let pokemon: PokemonDTO
   let dimensions: Double = 200
   
@@ -25,6 +35,39 @@ struct DetailView: View {
       Text(pokemon.name.capitalized)
         .bold()
         .font(.title2)
+      
+      VStack(spacing: 10) {
+        Text("**ID**: \(viewModel.pokemonDetail?.id ?? 0)")
+        Text("**Weight**: \(viewModel.pokemonDetail?.formatedWeight ?? "???") KG")
+        Text("**Height**: \(viewModel.pokemonDetail?.formatedHeight ?? "???") M")
+      }
+      .padding()
+    }
+  }
+}
+
+extension DetailView {
+  class ViewModel: ObservableObject {
+    
+    private let dataStore = PokemonDataStore()
+    
+    @Published var pokemonDetail: PokemonDetailDTO?
+    
+    func getDetails(pokemon: PokemonDTO) {
+      pokemonDetail = PokemonDetailDTO(id: 0, height: 0, weight: 0)
+      
+      dataStore.getPokemonDetail(id: pokemon.index) { data in
+        DispatchQueue.main.async {
+          self.pokemonDetail = data
+        }
+      }
+    }
+    
+    func formatHW(value: Int) -> String {
+      let dValue = Double(value)
+      let string = String(format: "%.2f", dValue / 10)
+      
+      return string
     }
   }
 }
